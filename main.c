d510c00be2319c5eea0dbae463237e299d4652cb
Fyrgeit <mans@peronilla.se>
2023-10-05 11:42:06 +0200

    Auto render scale

--- main.c
+++ main.c
@@ -10,41 +10,50 @@
 #define RENDER_WIDTH_PIXELS RENDER_WIDTH_TILES * TILE_SIZE_PIXELS
 #define RENDER_HEIGHT_PIXELS RENDER_HEIGHT_TILES * TILE_SIZE_PIXELS
 
-Texture GameSprites;
-
-int load()
+//Returns the largest scale the render could be without overflowing the window
+short UpdateRenderScale(short windowWidth, short windowHeight)
 {
-    GameSprites = LoadTexture("sprites/spritesheet.png");
+    short renderScale = 1;
+    
+    while (
+        RENDER_WIDTH_PIXELS * (renderScale + 1) <= windowWidth &&
+        RENDER_HEIGHT_PIXELS * (renderScale + 1) <= windowHeight
+    )
+    {
+        renderScale++;
+    }
+
+    return renderScale;
 }
 
 int main()
 {
     Tile map[256 * 6];
-
-    const short windowWidth = RENDER_WIDTH_PIXELS * 4;
-    const short windowHeight = RENDER_HEIGHT_PIXELS * 4;
     
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    short windowWidth = RENDER_WIDTH_PIXELS * 4.5;
+    short windowHeight = RENDER_HEIGHT_PIXELS * 4.5;
+
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE); //Make the window resizeble
     InitWindow(windowWidth, windowHeight, "Journey-Bound window testing");
 
-    RenderTexture2D renderTexture = LoadRenderTexture(RENDER_WIDTH_PIXELS, RENDER_HEIGHT_PIXELS);
-    Texture2D tileTexture = LoadTexture("sprites/texture.png");
+    RenderTexture2D renderTexture = LoadRenderTexture(RENDER_WIDTH_PIXELS, RENDER_HEIGHT_PIXELS); //The game will be rendered to a texture before being scaled to fit the players window
+    short renderScale = UpdateRenderScale(windowWidth, windowHeight);
 
-    short renderScale = 1;
+    Texture2D GameSprites = LoadTexture("sprites/spritesheet.png");
+    Texture2D tileTexture = LoadTexture("sprites/texture.png");
 
     while (!WindowShouldClose())
     {
-        if (IsWindowResized())
-        {
-            
-        }
+        windowWidth = GetScreenWidth();
+        windowHeight = GetScreenHeight();
         
-        if (IsKeyPressed(KEY_RIGHT)) renderScale++;
-        if (IsKeyPressed(KEY_LEFT)) renderScale--;
+        if (IsWindowResized()) renderScale = UpdateRenderScale(windowWidth, windowHeight);
         
+        //===Draw game content===
         BeginTextureMode(renderTexture);
         ClearBackground(LIGHTGRAY);
         
+        //Draw tiles
         for (short y = 0; y < RENDER_HEIGHT_TILES; y++)
         {
             for (short x = 0; x < RENDER_WIDTH_TILES; x++)
@@ -58,12 +67,20 @@ int main()
         
         EndTextureMode();
         
+        //===Draw render and debug===
         BeginDrawing();
         ClearBackground(BLACK);
 
         DrawTextureEx(renderTexture.texture, Vector2Zero(), 0, renderScale, WHITE);
+
+        //Debug drawing
+        for (short x = 0; x < windowWidth; x += RENDER_WIDTH_PIXELS)
+        { DrawLine(x, 0, x, windowHeight, RED); }
+        for (short y = 0; y < windowHeight; y += RENDER_HEIGHT_PIXELS)
+        { DrawLine(0, y, windowWidth, y, RED); }
         
         DrawText(TextFormat("FPS: %i", GetFPS()), 5, 5, 15, WHITE);
+
         EndDrawing();
     }
 
