de6a536a768e5f15fad70fd320eb00051c5ec709
Fyrgeit <mans@peronilla.se>
2023-10-05 13:34:41 +0200

    minor changes

--- main.c
+++ main.c
@@ -7,18 +7,17 @@
 #define TILE_SIZE_PIXELS (short)16
 #define RENDER_WIDTH_TILES (short)18
 #define RENDER_HEIGHT_TILES (short)12
-#define RENDER_WIDTH_PIXELS RENDER_WIDTH_TILES * TILE_SIZE_PIXELS
-#define RENDER_HEIGHT_PIXELS RENDER_HEIGHT_TILES * TILE_SIZE_PIXELS
+#define RENDER_WIDTH_PIXELS RENDER_WIDTH_TILES *TILE_SIZE_PIXELS
+#define RENDER_HEIGHT_PIXELS RENDER_HEIGHT_TILES *TILE_SIZE_PIXELS
 
-//Returns the largest scale the render could be without overflowing the window
-short UpdateRenderScale(short windowWidth, short windowHeight)
+// Returns the largest scale the render could be without overflowing the window
+short GetMaxRenderScale(short windowWidth, short windowHeight)
 {
     short renderScale = 1;
-    
+
     while (
         RENDER_WIDTH_PIXELS * (renderScale + 1) <= windowWidth &&
-        RENDER_HEIGHT_PIXELS * (renderScale + 1) <= windowHeight
-    )
+        RENDER_HEIGHT_PIXELS * (renderScale + 1) <= windowHeight)
     {
         renderScale++;
     }
@@ -29,15 +28,15 @@ short UpdateRenderScale(short windowWidth, short windowHeight)
 int main()
 {
     Tile map[256 * 6];
-    
+
     short windowWidth = RENDER_WIDTH_PIXELS * 4.5;
     short windowHeight = RENDER_HEIGHT_PIXELS * 4.5;
 
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE); //Make the window resizeble
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE); // Make the window resizeble
     InitWindow(windowWidth, windowHeight, "Journey-Bound window testing");
 
-    RenderTexture2D renderTexture = LoadRenderTexture(RENDER_WIDTH_PIXELS, RENDER_HEIGHT_PIXELS); //The game will be rendered to a texture before being scaled to fit the players window
-    short renderScale = UpdateRenderScale(windowWidth, windowHeight);
+    RenderTexture2D renderTexture = LoadRenderTexture(RENDER_WIDTH_PIXELS, RENDER_HEIGHT_PIXELS); // The game will be rendered to a texture before being scaled to fit the players window
+    short renderScale = GetMaxRenderScale(windowWidth, windowHeight);
 
     Texture2D GameSprites = LoadTexture("sprites/spritesheet.png");
     Texture2D tileTexture = LoadTexture("sprites/texture.png");
@@ -46,14 +45,16 @@ int main()
     {
         windowWidth = GetScreenWidth();
         windowHeight = GetScreenHeight();
-        
-        if (IsWindowResized()) renderScale = UpdateRenderScale(windowWidth, windowHeight);
-        
+
+        // Update the render scale when the window size changes
+        if (IsWindowResized())
+            renderScale = GetMaxRenderScale(windowWidth, windowHeight);
+
         //===Draw game content===
         BeginTextureMode(renderTexture);
         ClearBackground(LIGHTGRAY);
-        
-        //Draw tiles
+
+        // Draw tiles
         for (short y = 0; y < RENDER_HEIGHT_TILES; y++)
         {
             for (short x = 0; x < RENDER_WIDTH_TILES; x++)
@@ -64,9 +65,9 @@ int main()
                 }
             }
         }
-        
+
         EndTextureMode();
-        
+
         //===Draw render and debug===
         BeginDrawing();
         ClearBackground(BLACK);
@@ -74,20 +75,18 @@ int main()
         DrawTextureEx(
             renderTexture.texture,
             (Vector2){
-                (windowWidth - renderScale * RENDER_WIDTH_PIXELS) / 2,
-                (windowHeight - renderScale * RENDER_HEIGHT_PIXELS) / 2
-            },
+                (windowWidth - renderScale * RENDER_WIDTH_PIXELS) * 0.5,
+                (windowHeight - renderScale * RENDER_HEIGHT_PIXELS) * 0.5},
             0,
             renderScale,
-            WHITE
-        );
+            WHITE);
 
-        //Debug drawing
+        // Debug drawing
         /* for (short x = 0; x < windowWidth; x += RENDER_WIDTH_PIXELS)
         { DrawLine(x, 0, x, windowHeight, RED); }
         for (short y = 0; y < windowHeight; y += RENDER_HEIGHT_PIXELS)
         { DrawLine(0, y, windowWidth, y, RED); } */
-        
+
         DrawText(TextFormat("FPS: %i", GetFPS()), 5, 5, 15, WHITE);
 
         EndDrawing();
